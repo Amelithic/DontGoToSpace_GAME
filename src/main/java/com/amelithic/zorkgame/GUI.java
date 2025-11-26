4965aada64586f968e600692224dd4e999a72915
Amelithic <amelithic@gmail.com>
2025-11-26 22:58:50 +0000

    Basic GUI FXML layout - to be implemented

--- src/main/java/com/amelithic/zorkgame/GUI.java
+++ src/main/java/com/amelithic/zorkgame/GUI.java
@@ -1,5 +1,30 @@
 package com.amelithic.zorkgame;
 
-public class GUI {
-    
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("/gui/resources/gameScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Image icon = new Image(GUI.class.getResource("/images/icon.png").toExternalForm());
+        stage.getIcons().add(icon);
+
+        stage.setResizable(false);
+
+        stage.setTitle("Hello!");
+        stage.setScene(scene); //add to stage
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
