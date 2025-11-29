5978e56c35911d6bbf2a141ca5f9bd1d2e0d4c08
Amelithic <amelithic@gmail.com>
2025-11-29 23:54:46 +0000

    Screen switching (WIP) + split GUIControllers for each FXML from abstract class

--- src/main/java/com/amelithic/zorkgame/GUI.java
+++ src/main/java/com/amelithic/zorkgame/GUI.java
@@ -4,25 +4,40 @@ import java.io.IOException;
 
 import com.amelithic.zorkgame.characters.Player;
 import com.amelithic.zorkgame.gui.GUIController;
+import com.amelithic.zorkgame.gui.TitleController;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
+    protected Stage stage;
+    protected Scene scene;
+    protected Parent root;
+
+    protected Main gameState;
+    protected CommandManager commandManager;
+    protected Player player;
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        Main gameState = new Main();
-        CommandManager commandManager = new CommandManager();
-        Player player = gameState.getPlayer();
-        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("/gui/resources/gameScreen.fxml"));
+        this.stage = stage;
+
+        gameState = new Main();
+        commandManager = new CommandManager();
+        player = gameState.getPlayer();
+
+        //first screen -> title screen
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("/gui/resources/titleScreen.fxml"));
 
         // Inject dependencies into controller
         fxmlLoader.setControllerFactory(param -> {
-            if (param == GUIController.class) {
-                return new GUIController(gameState, player, commandManager);
+            if (param == TitleController.class) {
+                return new TitleController(this, gameState, player, commandManager);
             } else {
                 try {
                     return param.getDeclaredConstructor().newInstance();
@@ -32,21 +47,46 @@ public class GUI extends Application {
             }
         });
 
-        Scene scene = new Scene(fxmlLoader.load());
+        this.root = fxmlLoader.load();
+        scene = new Scene(root);
         String css = this.getClass().getResource("/gui/resources/gameScreen.css").toExternalForm(); 
         scene.getStylesheets().add(css);
 
         Image icon = new Image(GUI.class.getResource("/images/icon.png").toExternalForm());
         stage.getIcons().add(icon);
 
-        stage.setResizable(false);
-
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene); //add to stage
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void switchScreen(String fxmlPath) throws IOException {
+        FXMLLoader loader = new FXMLLoader(GUI.class.getResource(fxmlPath));
+
+        loader.setControllerFactory(param -> {
+            try {
+                // If the requested controller is a subtype of GUIController
+                if (GUIController.class.isAssignableFrom(param)) {
+                    // Find a constructor that matches your dependencies
+                    return param.getConstructor(GUI.class, Main.class, Player.class, CommandManager.class)
+                                .newInstance(this, gameState, player, commandManager);
+                } else {
+                    // Default: no-arg constructor
+                    return param.getDeclaredConstructor().newInstance();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Parent newRoot = loader.load();
+
+        // Instead of creating a new Scene, just swap the root
+        scene.setRoot(newRoot);
     }
 }
